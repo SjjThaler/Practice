ccfef1428f295bd9284b3687aa57c570a4f3a3e9
SjjThaler <stefanthaler@protonmail.com>
2024-01-27 20:30:56 +0100

    input modification

--- Maths problems/bogenmaß.py
+++ Maths problems/bogenmaß.py
@@ -1,4 +1,7 @@
-pi=22/7
+pi = 22/7
 degree = float(input("Input degrees: "))
-radian = degree*(pi/180)
+radian = degree * (pi / 180)
 print(radian)
+
+# Add input prompt to keep the terminal window open
+input("Press Enter to exit...")
