7d345c17a1a9fbb623325bcb0d3b2d546abe4739
SjjThaler <stefanthaler@protonmail.com>
2024-01-30 00:55:42 +0100

    Added main loop

--- Own projects/tic_tac_toe.py
+++ Own projects/tic_tac_toe.py
@@ -1,3 +1,4 @@
+#Define the Field class
 class Field:
 	def __init__(self, value, color):
 		self.value = value
@@ -16,6 +17,7 @@ class Field:
 		print(f7.color,f8.color,f9.color)
 
 
+#Define each field
 f1 = Field(False, "-")
 f2 = Field(False, "-")
 f3 = Field(False, "-")
@@ -27,8 +29,62 @@ f8 = Field(False, "-")
 f9 = Field(False, "-")
 
 
+#Define winning condition and turn
+victory = False
+turn = False
 
-f1.white()
+#Main Loop
+while victory == False:
+	if turn == False:
+		turn = True
+		print("Spieler 1 gibt ein: ")
+		x = input()
+		if x == "f1":
+			f1.black()
+		if x == "f2":
+			f2.black()
+		if x == "f3":
+			f3.black()
+		if x == "f4":
+			f4.black()
+		if x == "f5":
+			f5.black()
+		if x == "f6":
+			f6.black()
+		if x == "f7":
+			f7.black()
+		if x == "f8":
+			f8.black()
+		if x == "f9":
+			f9.black()
+
+	if turn == True:
+		turn = False
+		print("Spieler 2 gibt ein: ")
+		x = input()
+		if x == "f1":
+			f1.white()
+		if x == "f2":
+			f2.white()
+		if x == "f3":
+			f3.white()
+		if x == "f4":
+			f4.white()
+		if x == "f5":
+			f5.white()
+		if x == "f6":
+			f6.white()
+		if x == "f7":
+			f7.white()
+		if x == "f8":
+			f8.white()
+		if x == "f9":
+			f9.white()
+		
+	
+
+
+
 
 
 input("Press any key to close the application.")
