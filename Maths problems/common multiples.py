f13deb0354ea6b1c1e57670ef4e4d05f989fd47a
SjjThaler <stefanthaler@protonmail.com>
2024-02-01 22:11:02 +0100

    Algorithm for calculating common multiples

---
+++ Maths problems/common multiples.py
@@ -0,0 +1,21 @@
+
+sumlist = []
+
+n = 0
+
+endthree = int(1000 / 3)
+endfive = int(1000 / 5 - 1)
+
+while n < endthree:
+	n += 1
+	sumlist.append(n*3)
+
+n = 0
+
+while n < endfive:
+	n += 1
+	if n*5 not in sumlist:
+		sumlist.append(n*5)
+		
+
+print(sum(sumlist))
