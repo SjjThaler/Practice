7e65eeabfd3a2a05b0bb9abccf2da64553c9fc94
SjjThaler <stefanthaler@protonmail.com>
2024-02-07 21:42:54 +0100

    Learned how to access elements in nested lists

--- Own projects/chess (work in progress).py
+++ Own projects/chess (work in progress).py
@@ -18,10 +18,26 @@ class ChessBoard:
     	for row in self.board:
     		print(''.join(row))
 
+    def move_piece(self, from_pos, to_pos):
+
+    	# Unpacking tulples
+    	from_row, from_col = from_pos
+    	to_row, to_col = to_pos
+
+    	# Assigning the new position of the piece
+    	self.board[to_row][to_col] = self.board[from_row][from_col]
+
+    	# Clearing old position of the piece
+    	self.board[from_row][from_col] = '_'
+
 # Initialize chess board
 
 chess_board = ChessBoard()
 
 # Call chess_board
+
+chess_board.display()
+
+chess_board.move_piece((1,0),(3,0))
 
 chess_board.display()
