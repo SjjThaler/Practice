cef4942b6a853acc65ab8134a61ff84341da0608
SjjThaler <stefanthaler@protonmail.com>
2024-02-11 20:18:27 +0100

    Experimented with nested lists and [::] syntax

--- First_steps/nested_lists.py
+++ First_steps/nested_lists.py
@@ -9,4 +9,17 @@ x = nested_list[0][0]
 y = nested_list[1][2]
 z = nested_list[2][2]
 
-print(x,y,z)  
+print(x,y,z)
+
+# Trying out [::] on nested lists
+
+reverse = nested_list[::-1]
+double_reverse = nested_list[0][::-1]
+full_reverse = nested_list[::-1][::-1]
+
+print(reverse)  
+print(double_reverse)
+
+# Fully reversing both the list and the sublists doesn't
+# seem to be possible this way
+print(full_reverse)
