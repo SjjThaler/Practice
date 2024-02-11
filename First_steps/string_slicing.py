a07680b866588743f237dd1ae9282fbb29e8f498
SjjThaler <stefanthaler@protonmail.com>
2024-02-11 20:10:35 +0100

    Experimented with [::] syntax

--- First_steps/string_slicing.py
+++ First_steps/string_slicing.py
@@ -5,3 +5,14 @@ reverse = string [::-1]
 print(string)
 print(slice)
 print(reverse)
+
+numbers = "12345678910"
+
+extract_every = numbers[::1]
+halfreverse = numbers[5::-1]
+upperhalf = numbers[5:11:1]
+
+print(extract_every)
+print(halfreverse)
+print(upperhalf)
+
