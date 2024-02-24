e70d83e74a59e1edd39a0e9546f53c45dd963ca3
SjjThaler <stefanthaler@protonmail.com>
2024-02-24 12:25:40 +0100

    Changed to logistic regression and accuracy evaluation.

--- Learning/Machine Learning/musical_quality_prediction.py
+++ Learning/Machine Learning/musical_quality_prediction.py
@@ -1,17 +1,18 @@
 # Importing necessary libraries
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 
-# Sample data: house sizes (in square feet) and their corresponding prices (in thousands of dollars)
-chords = np.array([(1,3,5,7), (1,3,5,6.5), (1,2.5,5), (1,2.5,4.5), (1,3,5,7,9)]).reshape(-1, 1)  # Reshape to make it a 2D array
+# Sample data: musical chords and their corresponding qualities
+chords = np.array([(1,3,5,7), (1,3,5,6.5), (1,2.5,5), (1,2.5,4.5), (1,3,5,7,9)])
 qualities = np.array(["spheric", "dominant", "sad", "unstable", "colorful"])
 
 # Splitting the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(chords, qualities, test_size=0.2, random_state=42)
 
-# Creating and training the model
-model = LinearRegression()
+# Apparently linear regression doesn't work here because it expects continues values
+# Instead logistic regression can be used for classification tasks
+model = LogisticRegression()
 model.fit(X_train, y_train)
 
 # Making predictions on the test set
@@ -20,6 +21,6 @@ predictions = model.predict(X_test)
 # Printing the predictions
 print("Predictions:", predictions)
 
-# Evaluating the model (optional)
-score = model.score(X_test, y_test)
-print("Model Score:", score)
+# Can't use score to evaluate here as accuracy is more suitable for classification tasks
+accuracy = model.score(X_test, y_test)
+print("Model Accuracy:", accuracy)
