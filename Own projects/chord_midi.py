0688ff3c03dd365a0b3b356cece88b264b40055b
SjjThaler <stefanthaler@protonmail.com>
2024-03-02 11:04:26 +0100

    Added commentary.

--- Own projects/chord_midi.py
+++ Own projects/chord_midi.py
@@ -1,14 +1,14 @@
 from music21 import chord, stream
 
-# Define the chords you want to generate
-chord_names = [["C5","Eb","G"],"C", "D", "E", "F", "G", "A", "B"]
+# Define all the musical notes to operate on
 noten = ["C4","D4","E4","F4","G4","A4","B4","C5","D5","E5","F5","G5","A5","B5"]
 chords = {}
 
+# Generate chords by iterating through noten stopping at the seventh position
 for i in noten:
     chords[i]= noten[noten.index(i):noten.index(i)+7:2]
 
-# Create a stream to store the chords
+# Create a stream to store the seventh chords
 chords_stream = stream.Stream()
 
 # Generate chords and add them to the stream
@@ -16,7 +16,7 @@ for i in chords.values():
     new_chord = chord.Chord(i)
     chords_stream.append(new_chord)
 
-# Save the chords to separate MIDI files
+# Save the chords to separate MIDI files using the key of the chords dictionary
 for i, c in zip(chords.keys(), chords_stream):
     filename = f"{i}.wav"
     c.write('midi', fp=filename)
